a91c44bed3cdc7b504644739328f5878abc35082
Samuel Lee <71021677+simpleesamm@users.noreply.github.com>
2022-01-13 08:30:03 +0800

    Add files via upload

--- autoclicker.py
+++ autoclicker.py
@@ -13,10 +13,14 @@ import time
 
 #for tele
 import telegram_send
+import telebot
 
 #for mouse logger
 from pynput.mouse import Listener
 import logging
+
+#thread
+import threading
 
 
 #  ======== settings ========
@@ -57,8 +61,20 @@ def display_controls():
     print("-----------------------------------------------------")
     print('Press F1 to start ...')
 
-def detectChanges(xyList):
-    
+def detectChanges():
+
+
+    textFileHandling()
+
+    #2DArrayFormat
+    #0: To click on teams
+    #1: Top Left Bounding Box
+    #2: Bottom Right Bounding Box
+    #3: First Chat
+    #4: Second Chat
+    #5: Third Chat
+    #6: Idle Clicking Position
+
     while True: # http://effbot.org/zone/pil-comparing-images.htm
         
         im = grab(bbox=(int(xyList[1][0]), int(xyList[1][1]), int(xyList[2][0]), int(xyList[2][1])))
@@ -74,7 +90,7 @@ def detectChanges(xyList):
             yCenter = (bbox[3]- bbox[1])/2
             print(xCenter, yCenter)
             pyautogui.click(int(xyList[3][0]), int(xyList[3][1]))
-            time.sleep(2)
+            time.sleep(1)
             finalScreenshot = grab()
             finalScreenshot = finalScreenshot.save("image.jpg")
             
@@ -83,7 +99,7 @@ def detectChanges(xyList):
                 telegram_send.send(images=[f])
         
         else:
-            pyautogui.click(int(xyList[4][0]), int(xyList[4][1]))
+            pyautogui.click(int(xyList[6][0]), int(xyList[6][1]))
             
 
             
@@ -109,23 +125,94 @@ def mouselogger():
 
     with Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
         listener.join()
-        
 
-        
+def telebotReply():
+
+    #2DArrayFormat
+    #0: To click on teams
+    #1: Top Left Bounding Box
+    #2: Bottom Right Bounding Box
+    #3: First Chat
+    #4: Second Chat
+    #5: Third Chat
+    #6: Idle Clicking Position
+
+    bot = telebot.TeleBot("5042340263:AAEJv2Q5hSexHrng_Koqhqc4AzeZ2XF0XTs")
+
+    def handle_messages(messages):
+        for message in messages:
+            # Do something with the message
+            print(message.text)
+            bot.reply_to(message, 'Message Received')
+            messageHolder = message.text
+
+            #message format: #1:Your message (this means the first chat)
+            #message format: #2:Your message (this means the second chat)
+            #message format: #3:Your message (this means the third chat)
+
+            if messageHolder[0] == "#":
+                if messageHolder[1] == "1":
+                    splitMessage = messageHolder.split(":")
+                    pyautogui.click(int(xyList[3][0]), int(xyList[3][1]))
+                    time.sleep(1)
+                    pyautogui.write(splitMessage[1])
+                    time.sleep(1)
+                    pyautogui.press('enter')
+                
+                elif messageHolder[1] == "2":
+                    splitMessage = messageHolder.split(":")
+                    pyautogui.click(int(xyList[4][0]), int(xyList[4][1]))
+                    time.sleep(1)
+                    pyautogui.write(splitMessage[1])
+                    time.sleep(1)
+                    pyautogui.press('enter')
+
+                elif messageHolder[1] == "3":
+                    splitMessage = messageHolder.split(":")
+                    pyautogui.click(int(xyList[5][0]), int(xyList[5][1]))
+                    time.sleep(1)
+                    pyautogui.write(splitMessage[1])
+                    time.sleep(1)
+                    pyautogui.press('enter')
+                
+            else: #default first
+                    pyautogui.click(int(xyList[3][0]), int(xyList[3][1]))
+                    time.sleep(1)
+                    pyautogui.write(messageHolder)
+                    time.sleep(1)
+                    pyautogui.press('enter')
+
+                
+
+            
 
 
-def main():
+    bot.set_update_listener(handle_messages)
+    bot.infinity_polling()
+
+def textFileHandling():
+    
     os.remove("mouse_log.txt")
+    
     mouselogger()
     with open("mouse_log.txt", "r") as a_file:
         for line in a_file:
             stripped_line = line.strip()
-            #print(stripped_line)
-            splitList = stripped_line.split(",")
-            xyList.append(splitList)
+            if stripped_line[0].isalpha() == False:
+                #print(stripped_line[0].isalpha())
+                splitList = stripped_line.split(",")
+                xyList.append(splitList)
 
     print(xyList)
-    detectChanges(xyList)
+    
+
+
+def main():
+    t1 = threading.Thread(target=telebotReply)
+    t2 = threading.Thread(target=detectChanges)
+    t1.start()
+    t2.start()
+    
     
 
 
